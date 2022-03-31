949e24a9901475e6840c28c984834c8f2265e375
sweetpoison-android <you@example.comsk8996825@gmail.com>
2022-03-31 12:23:53 +0530

    add two number

--- app/src/main/java/com/wts/kotlinpractice/practice.kt
+++ app/src/main/java/com/wts/kotlinpractice/practice.kt
@@ -2,5 +2,7 @@ package com.wts.kotlinpractice
 
 fun main(args: Array<String>)
 {
-  println("Hello kotlin")
+//  println("Hello kotlin")
+  println(addNumber(5,10))
 }
+fun addNumber(a:Int, b:Int) = a+b;
